48c78aead2595b5f79d6811e2d1332820ec782e0
Emmanuel Temofe Anene <Eanene@wragbysolutions.com>
2019-04-30 19:04:36 +0100

    UI for stock

---
+++ HRM.ERP/Controllers/Accounting/NewStockController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRM.ERP.Controllers.Accounting
+{
+    public class NewStockController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AvailableStock()
+        {
+            return View();
+        }
+
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
